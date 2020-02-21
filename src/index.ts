27a450ac40750147d206be17f1ed0c26069ae367
carathorys <gallayb@gmail.com>
2020-02-21 07:56:13 +0100

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import 'zone.js/dist/zone-node';
 
-import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 import { join } from 'path';
 
-import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
-import { XmlRpcRepository } from 'src/server';
-import { Download } from 'src/server/models';
+import { XmlRpcRepository } from './server';
+import { Download } from './server/models';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
@@ -19,13 +16,7 @@ export function app() {
     ? 'index.original.html'
     : 'index';
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine(
-    'html',
-    ngExpressEngine({
-      bootstrap: AppServerModule,
-    }),
-  );
+  // server.engine('html');
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
@@ -82,9 +73,9 @@ export function app() {
   });
 
   // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
-  });
+  // server.get('*', (req, res) => {
+  //   res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+  // });
 
   return server;
 }
@@ -108,5 +99,3 @@ const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
-
-export * from './src/main.server';
