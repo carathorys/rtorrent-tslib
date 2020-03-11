fd0a00e5a66d429a40df1490c99ca30c7de0f206
carathorys <gallayb@gmail.com>
2020-03-11 11:49:47 +0100

    Changes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import * as process from 'process';
+
 export * from './RTorrentClient';
 export * from './models';
+
+
+if (typeof window !== 'undefined') {
+  (window as any).process = process;
+}
